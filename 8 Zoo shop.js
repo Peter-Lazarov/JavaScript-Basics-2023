7dd842c366ad26fbbd658d72d8be3fb3c5963302
Peter Lazarov <peter_lazarov@hotmail.com>
2023-05-11 18:17:28 +0300

    Create 8 Zoo shop.js

---
+++ 8 Zoo shop.js
@@ -0,0 +1,9 @@
+function zooshop(input) {
+    let dogFood = Number(input[0]);
+    let catFood = Number(input[1]);
+    let sum = (dogFood * 2.5) + (catFood * 4);
+
+    console.log(sum + " lv.");
+}
+
+zooshop(["5", "4"]);
